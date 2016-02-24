2fdfd99efa4bd409b5831b0730bdca0261907bc4
Evan McNaughtan <evan4james@yahoo.com>
2016-02-24 12:40:37 -0800

    sorry

--- src/ArrayThread.java
+++ src/ArrayThread.java
@@ -5,56 +5,57 @@ import java.util.concurrent.locks.Lock;
 
 public class ArrayThread extends Thread {
 
-    private final Lock lock;
-    private final Condition condition;
-    private final int NUM_OPERATIONS = 50;
-    private String[] array, pool;
+   private final Lock lock;
+   private final Condition condition;
+   private final int NUM_OPERATIONS = 50;
+   private String[] array, pool;
 
-    public ArrayThread(Lock lock, Condition condition, String[] array, String[] pool) {
-        this.lock = lock;
-        this.condition = condition;
-        this.array = array;
-        this.pool = pool;
-    }
+   public ArrayThread(Lock lock, Condition condition, String[] array,
+   String[] pool) {
+      this.lock = lock;
+      this.condition = condition;
+      this.array = array;
+      this.pool = pool;
+   }
 
-    public void performTask(int operation) {
-        if (operation >= 0 && operation <= 999) {
-            searchForString();
-        } else {
-            replaceString();
-        }
-    }
+   public void performTask(int operation) {
+      if (operation >= 0 && operation <= 999) {
+         searchForString();
+      } else {
+         replaceString();
+      }
+   }
 
-    public void searchForString() {
-        String poolString = pool[new Random().nextInt(pool.length)];
-        lock.lock();
-        try {
-            for (int i = array.length - 1; i > 0; i--) {
-                if (array[i].equals(poolString)) {
-                    System.out.printf("The last occurrence of %s is at index %d"
-                     , poolString, i);
-                    break;
-                }
+   public void searchForString() {
+      String poolString = pool[new Random().nextInt(pool.length)];
+      lock.lock();
+      try {
+         for (int i = array.length - 1; i > 0; i--) {
+            if (array[i].equals(poolString)) {
+               System.out.printf("The last occurrence of %s is at index %d",
+               poolString, i);
+               break;
             }
-        } finally {
-            lock.unlock();
-        }
-    }
+         }
+      } finally {
+         lock.unlock();
+      }
+   }
 
-    public void replaceString() {
-        int index = new Random().nextInt(array.length);
+   public void replaceString() {
+      int index = new Random().nextInt(array.length);
 
-        lock.lock();
-        try {
-            array[index] = pool[new Random().nextInt(pool.length)];
-        } finally {
-            lock.unlock();
-        }
-    }
+      lock.lock();
+      try {
+         array[index] = pool[new Random().nextInt(pool.length)];
+      } finally {
+         lock.unlock();
+      }
+   }
 
-    public void run() {
-        for (int i = 0; i < NUM_OPERATIONS; i++) {
-            performTask(new Random().nextInt(1101));
-        }
-    }
+   public void run() {
+      for (int i = 0; i < NUM_OPERATIONS; i++) {
+         performTask(new Random().nextInt(1101));
+      }
+   }
 }
--- src/ThreadMain.java
+++ src/ThreadMain.java
@@ -3,13 +3,40 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+import java.util.Random;
 
 public class ThreadMain {
-    public static void main(String[] args) {
-        Lock lock = new ReentrantLock();
-        Condition condition = lock.newCondition();
-        
-        System.out.println("hi");
-        
-    }
+
+   public static void main(String[] args) {
+      final int NUM_OF_THREADS = 20;
+      
+      Lock lock = new ReentrantLock();
+      Condition condition = lock.newCondition();
+      Thread[] threadArray = new Thread[NUM_OF_THREADS];
+
+      String[] POOL = new String[110];
+      String[] ARRAY= new String[100];
+
+      for (int i = 0; i < POOL.length; i++) {
+         POOL[i] = generateRandomString();
+      }
+      
+      for (int i = 0; i < ARRAY.length; i++) {
+         ARRAY[i] = POOL[new Random().nextInt(POOL.length)];
+      }
+      
+      for (int i = 0; i < threadArray.length; i++) {
+         
+      }
+   }
+
+   public static String generateRandomString() {
+      int lengthOfString = new Random().nextInt(9) + 1;
+      String randomString = "";
+      
+      for (int i = 0; i < lengthOfString; i++) {
+         randomString += (char)(new Random().nextInt(26) + 'A');
+      }
+      return randomString;
+   }
 }
